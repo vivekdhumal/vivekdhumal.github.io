34429ef3f97112404ae13568260770759c3055d2
Vivek Dhumal <vivekdhumalmail@gmail.com>
2018-07-14 19:03:21 +0530

    Total refreshing site
    Add a single introductory page.

---
+++ webpack.mix.js
@@ -0,0 +1,37 @@
+let mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
+let glob = require('glob-all');
+let PurgecssPlugin = require("purgecss-webpack-plugin");
+
+/**
+ * Custom PurgeCSS Extractor
+ * https://github.com/FullHuman/purgecss
+ * https://github.com/FullHuman/purgecss-webpack-plugin
+ */
+class TailwindExtractor {
+    static extract(content) {
+      return content.match(/[A-z0-9-:\/]+/g);
+    }
+}
+
+mix.sass('resources/assets/css/app.scss', 'assets/css')
+    .options({
+        processCssUrls: false,
+        postCss: [ tailwindcss('./tailwind-config.js') ],
+    });
+
+mix.webpackConfig({
+    plugins: [
+        new PurgecssPlugin({
+            paths: glob.sync([
+                path.join(__dirname, "*.html"),
+            ]),
+            extractors: [
+                {
+                extractor: TailwindExtractor,
+                extensions: ["html", "js"]
+                }
+            ]
+        })
+    ]
+});
