981d382d5de37424430709d5bdc13b8a6e03e45b
Vivek Dhumal <vivekdhumalmail@gmail.com>
2024-11-18 20:10:06 +0530

    Build for deploy

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'production' => false,
-    'baseUrl' => '',
-    'title' => 'Vivek Dhumal',
-    'description' => 'With over 12 years of expertise in web and mobile app development',
-    'collections' => [],
-];
